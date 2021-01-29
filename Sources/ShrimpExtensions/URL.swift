49aa0db632b90bb4538299687e6356344dd5e34f
kamaal111 <kamaal.f1@gmail.com>
2021-01-29 19:45:17 +0100

    Decoding json from URL

--- Sources/ShrimpExtensions/URL.swift
+++ Sources/ShrimpExtensions/URL.swift
@@ -11,4 +11,20 @@ public extension URL {
     init(staticString: StaticString) {
         self.init(string: "\(staticString)")!
     }
+
+    func getJSON<T: Codable>() -> Result<T, Error> {
+        var data: Data
+        do {
+            data = try Data(contentsOf: self, options: .mappedIfSafe)
+        } catch {
+            return .failure(error)
+        }
+        var file: T
+        do {
+            file = try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            return .failure(error)
+        }
+        return .success(file)
+    }
 }
