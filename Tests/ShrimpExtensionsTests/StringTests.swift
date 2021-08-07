2b227123da7559855dd8718817a46dbcadc6fe14
kamaal111 <kamaal.f1@gmail.com>
2021-08-07 18:54:38 +0200

    Testing strings extensions

---
+++ Tests/ShrimpExtensionsTests/StringTests.swift
@@ -0,0 +1,79 @@
+//
+//  StringTests.swift
+//  
+//
+//  Created by Kamaal M Farah on 07/08/2021.
+//
+
+import XCTest
+@testable import ShrimpExtensions
+
+class StringTests: XCTestCase { }
+
+// - MARK: splitByCapital
+
+extension StringTests {
+    func testSplitByCapital() {
+        let testCase = "IAmSuperCool".splitByCapital
+        XCTAssertEqual(testCase.count, 4)
+        let expectedResult = ["I", "Am", "Super", "Cool"]
+        XCTAssertEqual(testCase, expectedResult)
+    }
+}
+
+// - MARK: trimmingByWhitespacesAndNewLines
+
+extension StringTests {
+    func testTrimmingByWhitespacesAndNewLines() {
+        let testCase = " I am super cool ".trimmingByWhitespacesAndNewLines
+        let expectedResult = "I am super cool"
+        XCTAssertEqual(testCase, expectedResult)
+    }
+}
+
+// - MARK: nsString
+
+extension StringTests {
+    func testNSString() {
+        XCTAssertNotNil("nstring".nsString)
+    }
+}
+
+// - MARK: uuid
+
+extension StringTests {
+    func testUUID() {
+        XCTAssertNil("not a uuid".uuid)
+        XCTAssertNotNil("26e92a7d-b958-4106-8372-c1cceed40b1f".uuid)
+    }
+}
+
+// - MARK: scrambled
+
+extension StringTests {
+    func testScrambled() {
+        let original = "original"
+        let scrambled = original.scrambled
+        XCTAssertEqual(original.count, scrambled.count)
+    }
+}
+
+// - MARK: digits
+
+extension StringTests {
+    func testDigits() {
+        let testCase = "Cool story 4 the 2 foxes with 0 carrots".digits
+        let expectedResult = "420"
+        XCTAssertEqual(testCase, expectedResult)
+    }
+}
+
+// - MARK: replaceMultipleOccurrences
+
+extension StringTests {
+    func testReplaceMultipleOccurrences() {
+        let testCase = "yes 22".replaceMultipleOccurrences(of: ["e", "s"], with: "a")
+        let expectedResult = "yaa 22"
+        XCTAssertEqual(testCase, expectedResult)
+    }
+}
