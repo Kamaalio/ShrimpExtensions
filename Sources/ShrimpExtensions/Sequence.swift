0d66f5e4a5462d03cd24f7ad781e7b7faf640955
kamaal111 <kamaal.f1@gmail.com>
2021-12-24 14:24:53 +0100

    Documenting find method

--- Sources/ShrimpExtensions/Sequence.swift
+++ Sources/ShrimpExtensions/Sequence.swift
@@ -13,10 +13,30 @@ public extension Sequence {
         Array(self)
     }
 
+    /// Returns the first element of the sequence that satisfies the given key path and comparison value.
+    ///
+    /// - Complexity: O(*n*), where *n* is the length of the sequence.
+    ///
+    /// - Parameters:
+    ///   - keyPath: `KeyPath` of equatable type to search for.
+    ///   - comparisonValue: The comparison value to to match the condition to search for.
+    ///
+    /// - Returns: The first element of the sequence that satisfies the given key path
+    ///   and comparison value or nil if there is no element that satisfies the condition.
     func find<T: Equatable>(by keyPath: KeyPath<Element, T>, is comparisonValue: T) -> Element? {
         find(where: { $0[keyPath: keyPath] == comparisonValue })
     }
 
+    /// Returns the first element of the sequence that satisfies the given
+    /// predicate.
+    ///
+    /// - Complexity: O(*n*), where *n* is the length of the sequence.
+    ///
+    /// - Parameter predicate: A closure that takes an element of the sequence as
+    ///   its argument and returns a Boolean value indicating whether the
+    ///
+    /// - Returns: The first element of the sequence that satisfies `predicate`,
+    ///   or `nil` if there is no element that satisfies `predicate`.
     func find(where predicate: (Element) throws -> Bool) rethrows -> Element? {
         try self.first(where: predicate)
     }
