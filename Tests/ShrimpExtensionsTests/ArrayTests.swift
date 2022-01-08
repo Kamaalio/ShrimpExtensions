417ece7d80b5fcc16b22ceee07717741a4322ccb
kamaal111 <kamaal.f1@gmail.com>
2022-01-08 12:49:12 +0100

    Adding at method to array

--- Tests/ShrimpExtensionsTests/ArrayTests.swift
+++ Tests/ShrimpExtensionsTests/ArrayTests.swift
@@ -6,7 +6,49 @@
 //
 
 import XCTest
+import Quick
+import Nimble
+
 @testable import ShrimpExtensions
+
+final class ArraySpec: QuickSpec {
+    override func spec() {
+
+        // - MARK: At
+
+        describe("at") {
+            context("Gets elements with given index") {
+                let arr = (0..<3).asArray()
+                let cases = [
+                    (0, 0),
+                    (1, 1),
+                    (2, 2),
+                    (-1, 2),
+                    (-2, 1),
+                    (-3, 0),
+                ]
+                for (index, element) in cases {
+                    it("gets \(element) with the index of \(index) from \(arr)") {
+                        expect(arr.at(index)).to(equal(element))
+                    }
+                }
+            }
+            context("Fails to get given element because index is out of range") {
+                let arr = (0..<3).asArray()
+                let cases = [
+                    (-4),
+                    (3),
+                ]
+                for (index) in cases {
+                    it("fails to get with the index of \(index)") {
+                        expect(arr.at(index)).to(beNil())
+                    }
+                }
+            }
+        }
+
+    }
+}
 
 final class ArrayTests: XCTestCase { }
 
