25eb8ec480a4cd0ab2f9c3095d428d6e62839a9d
Kamaal <kamaal.f1@gmail.com>
2023-01-07 16:54:49 +0100

    Adding localized string to double to string

--- Sources/ShrimpExtensions/String.swift
+++ Sources/ShrimpExtensions/String.swift
@@ -45,6 +45,34 @@ extension StringProtocol {
         return chunks
     }
 
+    /// Transforming localized number to a double
+    public var localizedStringToDouble: Double? {
+        let string = self.trimmingByWhitespacesAndNewLines
+        guard let startNumberIndex = string.firstIndex(where: \.isNumber),
+              let endNumberIndex = string.lastIndex(where: \.isNumber) else { return nil }
+
+        let rawAmount = string[startNumberIndex...endNumberIndex]
+        let rawAmountCount = rawAmount.count
+        var seperatorIndex: Int?
+        let seperators: [Character] = [".", ","]
+        if rawAmountCount > 1 {
+            for index in 0..<rawAmountCount {
+                let characterIndex = rawAmountCount - index - 1
+                let character = rawAmount[characterIndex]
+                if seperators.contains(character) {
+                    seperatorIndex = characterIndex
+                    break
+                }
+            }
+        }
+
+        return Double(String(rawAmount
+            .enumerated()
+            .filter({ $0.offset == seperatorIndex || !seperators.contains($0.element) })
+            .map(\.element))
+            .replacingOccurrences(of: ",", with: "."))
+    }
+
     /// Split String by newlines(\n)
     public var splitLines: [SubSequence] {
         self.split(whereSeparator: \.isNewline)
